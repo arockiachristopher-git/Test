64a002129b5ee2d276ec875c96bfd3bd51e36d7f
arockiachristopher-git <102205690+arockiachristopher-git@users.noreply.github.com>
2022-05-01 09:30:38 +0530

    Update sum.py

--- sum.py
+++ sum.py
@@ -4,7 +4,7 @@ def sum(a,b):
 	return a + b
 
 def main():
-	first_variable = "20"
+	first_variable = "30"
 	second_variable = "20"
 	sum_value = sum(first_variable, second_variable)
 	print(sum_value)
